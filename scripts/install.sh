651480a7711f6bce892ae06d472c6efb78c650c8
Evertras <bfullj@gmail.com>
2022-01-18 10:53:15 +0900

    Install themes

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,3 +16,13 @@ link .bashrc
 link .tmux.conf
 link .vimrc
 
+mkdir -p ~/.bashrc.d/themes
+
+# Link all our themes
+for SRCFILE in ./.bashrc.d/themes/*; do
+	if [[ -f ${SRCFILE} ]]; then
+		THEME_FILE=.bashrc.d/themes/${SRCFILE##*/}
+		link ${THEME_FILE}
+	fi
+done
+

51eba6702601f81dd0a74b1a93dbca58518d76de
Brandon Fulljames <bfullj@gmail.com>
2023-07-07 00:37:05 +0900

    Add clear for search

--- etc/nvim/lua/evertras/remap.lua
+++ etc/nvim/lua/evertras/remap.lua
@@ -11,3 +11,6 @@ vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
 vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
 vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
 vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+
+-- Clear search
+vim.keymap.set("n", "<leader><space>", vim.cmd.nohlsearch)

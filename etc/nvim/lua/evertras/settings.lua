a257579fadc2da94571a8b1a1bd5277a959217fb
Brandon Fulljames <bfullj@gmail.com>
2023-07-07 00:46:13 +0900

    Use swap/backup again

--- etc/nvim/lua/evertras/settings.lua
+++ etc/nvim/lua/evertras/settings.lua
@@ -1,8 +1,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.swapfile= false
-vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 

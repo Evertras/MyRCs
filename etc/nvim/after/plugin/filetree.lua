608b5b2ffefa3e5fc2a52971ec5591758fd72b72
Brandon Fulljames <bfullj@gmail.com>
2023-07-07 01:48:54 +0900

    Add chadtree

---
+++ etc/nvim/after/plugin/filetree.lua
@@ -0,0 +1,3 @@
+--require("nvim-tree").setup()
+
+vim.keymap.set("n", "<C-N>", vim.cmd.CHADopen)
--- etc/nvim/lua/evertras/settings.lua
+++ etc/nvim/lua/evertras/settings.lua
@@ -1,5 +1,10 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+vim.opt.termguicolors = true
 
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true

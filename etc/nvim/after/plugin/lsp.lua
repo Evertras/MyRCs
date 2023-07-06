e5894be828f5c464643552446988266e51278599
Brandon Fulljames <bfullj@gmail.com>
2023-07-07 00:26:19 +0900

    Cleanup

--- etc/nvim/after/plugin/lsp.lua
+++ etc/nvim/after/plugin/lsp.lua
@@ -1,6 +1,6 @@
-local lsp = require('lsp-zero').preset("recommended")
+local lsp = require('lsp-zero').preset({})
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 
   local opts = {buffer = bufnr, remap = false}

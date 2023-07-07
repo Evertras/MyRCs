f3e39e8abe650228eb8f2749a271d9f5a8aa6cc0
Brandon Fulljames <bfullj@gmail.com>
2023-07-07 10:23:57 +0900

    Adjust colors to be easier to read on dark background

--- etc/nvim/after/plugin/colors.lua
+++ etc/nvim/after/plugin/colors.lua
@@ -1,20 +1,11 @@
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
     background = { -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = false, -- disables setting the background color.
+    transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
         conditionals = { "italic" },
@@ -30,7 +21,14 @@ require("catppuccin").setup({
         operators = {},
     },
     color_overrides = {},
-    custom_highlights = {},
+    custom_highlights = function (colors)
+      -- Show up on a dark wallpaper better
+      -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
+      return {
+        Comment = { fg = colors.peach },
+        LineNr = { fg = colors.sky },
+      }
+    end,
     integrations = {
         cmp = true,
         gitsigns = true,

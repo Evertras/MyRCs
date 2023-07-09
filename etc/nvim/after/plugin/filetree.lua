0712c7f7dde6cd0719020cd92aaafe96a00018c7
Brandon Fulljames <bfullj@gmail.com>
2023-07-09 11:37:02 +0900

    Easier split keybinds

--- etc/nvim/after/plugin/filetree.lua
+++ etc/nvim/after/plugin/filetree.lua
@@ -1,3 +1,20 @@
+local function custom_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical split'))
+  vim.keymap.set('n', 'h', api.node.open.horizontal, opts('Open: Horizontal split'))
+end
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
@@ -9,6 +26,7 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
+  on_attach = custom_attach,
 })
 
 vim.keymap.set("n", "<C-N>", vim.cmd.NvimTreeToggle)

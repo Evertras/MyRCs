5a52cfafa5a554ce9b1a5a9d5054a9168d12f1e6
Brandon Fulljames <bfullj@gmail.com>
2023-07-17 01:52:19 +0900

    Move telescope shortcuts to stop clashing

--- etc/nvim/after/plugin/telescope.lua
+++ etc/nvim/after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>fF', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gG', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
 
-vim.keymap.set('n', '<leader>fs', function()
+vim.keymap.set('n', '<leader>gs', function()
   builtin.grep_string({ search = vim.fn.input("ag > ") });
 end)

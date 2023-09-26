e4731fb6efefd54ff3e539cadc2c0b0630b1e6ff
Brandon Fulljames <bfullj@gmail.com>
2023-09-26 15:41:43 +0900

    Highlight quickscope better

---
+++ etc/nvim/after/plugin/quickscope.lua
@@ -0,0 +1,2 @@
+vim.cmd([[highlight QuickScopePrimary guifg='#afff5f' gui=bold ctermfg=155 cterm=underline]])
+vim.cmd([[highlight QuickScopeSecondary guifg='#5fffff' gui=bold ctermfg=81 cterm=underline]])

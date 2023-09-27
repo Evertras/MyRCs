a55a05d941b326662b6e29d4c60baacdc0e78d6e
Brandon Fulljames <bfullj@gmail.com>
2023-09-27 16:48:55 +0900

    Formatting

--- etc/nvim/after/plugin/colors.lua
+++ etc/nvim/after/plugin/colors.lua
@@ -1,44 +1,45 @@
+-- Customize catppuccin
 require("catppuccin").setup({
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = function (colors)
-      -- Show up on a dark wallpaper better
-      -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
-      return {
-        Comment = { fg = colors.peach },
-        LineNr = { fg = colors.sky },
-      }
-    end,
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        notify = false,
-        mini = false,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = {     -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true, -- disables setting the background color.
+  show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+  styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" },     -- Change the style of comments
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
+  color_overrides = {},
+  custom_highlights = function(colors)
+    -- Show up on a dark wallpaper better
+    -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
+    return {
+      Comment = { fg = colors.peach },
+      LineNr = { fg = colors.sky },
+    }
+  end,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    telescope = true,
+    notify = false,
+    mini = false,
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
 })
 
--- setup must be called before loading
+-- actually pick the color scheme
 vim.cmd.colorscheme "catppuccin"

23dd3b2547ac57faa822536b607778ed12b27b82
Brandon Fulljames <bfullj@gmail.com>
2023-12-08 11:09:08 +0900

    Apply our locale

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -60,7 +60,7 @@
   time.timeZone = "Asia/Tokyo";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";

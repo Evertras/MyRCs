3a6a5c9f0072b009993bdcdecb061567d73e86c3
Brandon Fulljames <bfullj@gmail.com>
2023-12-10 11:18:58 +0900

    Be more pure

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 # Basic nix shell to bring along some common stuff to use in non-NixOS places
-# Experimental...
+# Experimental... meant to be run with --pure
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
@@ -7,10 +7,14 @@ in
 
 pkgs.mkShell {
   packages = with pkgs; [
+    # Core important things for --pure
     git
+    openssh
+    which
+
+    # General stuff
     neovim
     starship
-    which
   ];
 
   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";

94abba5e58b30e947c7b200af3c2270533884ee4
Brandon Fulljames <bfullj@gmail.com>
2023-12-13 13:33:54 +0900

    Add reference for picom

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -42,6 +42,7 @@ if ! type i3; then
 fi
 
 if ! type picom; then
+  # https://github.com/yshui/picom#build
   sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
   rm -rf ~/.build-picom
   mkdir -p ~/.build-picom

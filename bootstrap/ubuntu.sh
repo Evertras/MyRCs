d6fad8cbcd686be8745d7432601ca26d26fbf8d1
Brandon Fulljames <bfullj@gmail.com>
2023-12-17 09:09:51 +0900

    Allow ubuntu headless bootstrap

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -19,56 +19,60 @@ mkdir -p ~/bin
 # so that we can install it with asdf or pyenv
 sudo apt install -y --no-upgrade build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev libbz2-dev libffi-dev
 
-# Rust (mostly for Alacritty)
-if ! type cargo; then
-  echo "DO NOT MODIFY BASHRC/PROFILE (or fix them later if you did)"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  reloadbash
-fi
+read -p "Install desktop environment? [y\n] " desktop
 
-# Desktop environment with i3
-if ! type i3; then
-  # i3 in Ubuntu 20.04 is old and doesn't support include, so just get latest
-  rm -rf ~/.everbuild-i3
-  mkdir -p ~/.everbuild-i3
-  pushd ~/.everbuild-i3
-    # https://i3wm.org/docs/repositories.html
-    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
-    sudo apt install ./keyring.deb
-    echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
-    sudo apt update
-    sudo apt install i3
-  popd
-  rm -rf ~/.everbuild-i3
-fi
+if [[ "${desktop}" =~ ^[Yy]$ ]]; then
+  # Rust (mostly for Alacritty)
+  if ! type cargo; then
+    echo "DO NOT MODIFY BASHRC/PROFILE (or fix them later if you did)"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    reloadbash
+  fi
 
-if ! type dmenu; then
-  sudo apt install -y dmenu
-fi
+  # Desktop environment with i3
+  if ! type i3; then
+    # i3 in Ubuntu 20.04 is old and doesn't support include, so just get latest
+    rm -rf ~/.everbuild-i3
+    mkdir -p ~/.everbuild-i3
+    pushd ~/.everbuild-i3
+      # https://i3wm.org/docs/repositories.html
+      /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+      sudo apt install ./keyring.deb
+      echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+      sudo apt update
+      sudo apt install i3
+    popd
+    rm -rf ~/.everbuild-i3
+  fi
 
-if ! type picom; then
-  # https://github.com/yshui/picom#build
-  sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
-  rm -rf ~/.everbuild-picom
-  mkdir -p ~/.everbuild-picom
-  pushd ~/.everbuild-picom
-    git clone https://github.com/yshui/picom
-    cd picom
+  if ! type dmenu; then
+    sudo apt install -y dmenu
+  fi
 
-    echo "Running meson setup"
-    meson setup --buildtype=release build
-    echo "Running ninja build"
-    ninja -C build
-    echo "Installing picom to ~/bin/picom"
-    mv build/src/picom ~/bin/picom
-  popd
-  rm -rf ~/.everbuild-picom
-fi
+  if ! type picom; then
+    # https://github.com/yshui/picom#build
+    sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
+    rm -rf ~/.everbuild-picom
+    mkdir -p ~/.everbuild-picom
+    pushd ~/.everbuild-picom
+      git clone https://github.com/yshui/picom
+      cd picom
 
-if ! type alacritty; then
-  # https://github.com/alacritty/alacritty/blob/master/INSTALL.md#dependencies
-  sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-  cargo install alacritty
+      echo "Running meson setup"
+      meson setup --buildtype=release build
+      echo "Running ninja build"
+      ninja -C build
+      echo "Installing picom to ~/bin/picom"
+      mv build/src/picom ~/bin/picom
+    popd
+    rm -rf ~/.everbuild-picom
+  fi
+
+  if ! type alacritty; then
+    # https://github.com/alacritty/alacritty/blob/master/INSTALL.md#dependencies
+    sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+    cargo install alacritty
+  fi
 fi
 
 ################################################################################
